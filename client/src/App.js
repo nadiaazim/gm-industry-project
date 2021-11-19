56535eae397da4a67a1c1f8b64857e5b85715706
Nadia Azim <nd.azim94@gmail.com>
2021-11-18 20:58:03 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+// import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+require("dotenv").config();
+const port = process.env.PORT || 8080
+
+app.use(cors());
+app.use(express.json())
+
+app.listen(8080, function () {
+    console.log("The server is now listening on 8080")
+});
+
