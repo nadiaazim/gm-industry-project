80e1190825f67595d9e5937262fa78820a320ddd
Thessa Kranendonk <thessakranendonk@MacBook-Pro-van-Thessa.local>
2021-11-19 14:51:30 -0500

    working modal and links

--- client/src/Components/LocationPage/LocationPage.jsx
+++ client/src/Components/LocationPage/LocationPage.jsx
@@ -5,7 +5,29 @@ import filter from "../../assets/icons/filter-icon.png";
 import arrow from "../../assets/icons/arrow-left.png";
 import "./LocationPage.scss";
 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "14rem",
+  bgcolor: "background.paper",
+  border: "none",
+  outline: "none",
+  borderRadius: "5px",
+  boxShadow: 24,
+  p: 4,
+};
+
 function LocationPage() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
       <section className="location">
@@ -25,13 +47,36 @@ function LocationPage() {
           <div className="location__button-wrap">
             <button className="location__open-now">OPEN NOW</button>
             <button className="location__closest-first">CLOSEST FIRST</button>
-            <Link to="/sort">
-              <img
-                className="location__filter"
-                src={filter}
-                alt="settings icon"
-              />
-            </Link>
+
+            <div className="sort">
+              <a onClick={handleOpen}>
+                <img
+                  className="location__filter"
+                  src={filter}
+                  alt="settings icon"
+                />
+              </a>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <h1 className="sort__header">Sort Results By</h1>
+                  <button className="sort__button sort__button--blue">
+                    OPEN NOW
+                  </button>
+                  <button className="sort__button">PUBLIC STATIONS ONLY</button>
+                  <button className="sort__button sort__button--blue">
+                    CLOSEST FIRST
+                  </button>
+                  <button className="sort__button">LOWEST RATE FIRST</button>
+                  <br />
+                  <button className="sort__button-save">SAVE CHANGES</button>
+                </Box>
+              </Modal>
+            </div>
           </div>
           <div className="location__map-wrap">
             <img className="location__map" src={map} alt="map" />
--- client/src/Components/Sort/Sort.jsx
+++ client/src/Components/Sort/Sort.jsx
@@ -1,21 +1,58 @@
-import './Sort.scss'
-
-const Sort = () => {
-    return (
-        <div className="sort">
-            <div className="sort__close-container">
-                <img className="sort__close-button"src="" alt="" />
-            </div>
-            <div className="sort__body">
-                <h1 className="sort__header">Sort Results By</h1>
-                <button className="sort__button sort__button--blue">OPEN NOW</button>
-                <button className="sort__button">PUBLIC STATIONS ONLY</button>
-                <button className="sort__button sort__button--blue">CLOSEST FIRST</button>
-                <button className="sort__button">LOWEST RATE FIRST</button>
-                <button className="sort__button-save">SAVE CHANGES</button>
-            </div>
-        </div>
-    )
-}
-
-export default Sort
+// import './Sort.scss'
+// ​
+// import * as React from "react";
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
+// import Modal from "@mui/material/Modal";
+// ​
+// const style = {
+//   position: "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: 400,
+//   bgcolor: "background.paper",
+//   border: "2px solid #000",
+//   boxShadow: 24,
+//   p: 4,
+// };
+// ​
+// const Sort = () => {
+// ​
+//     const [open, setOpen] = React.useState(false);
+//     const handleOpen = () => setOpen(true);
+//     const handleClose = () => setOpen(false);
+//     return (
+//       <div className="sort">
+//         <div className="sort__close-container">
+//           <img className="sort__close-button" src="" alt="" />
+//         </div>
+//         <div className="sort__body">
+//           <Button onClick={handleOpen}>Open modal</Button>
+//           <Modal
+//             open={open}
+//             onClose={handleClose}
+//             aria-labelledby="modal-modal-title"
+//             aria-describedby="modal-modal-description"
+//           >
+//             <Box sx={style}>
+//               <h1 className="sort__header">Sort Results By</h1>
+//               <button className="sort__button sort__button--blue">
+//                 OPEN NOW
+//               </button>
+//               <button className="sort__button">PUBLIC STATIONS ONLY</button>
+//               <button className="sort__button sort__button--blue">
+//                 CLOSEST FIRST
+//               </button>
+//                 <button className="sort__button">LOWEST RATE FIRST</button>
+//                 <br/>
+//               <button className="sort__button-save">SAVE CHANGES</button>
+//             </Box>
+//           </Modal>
+//         </div>
+//       </div>
+//     );
+// }
+// ​
+// export default Sort
