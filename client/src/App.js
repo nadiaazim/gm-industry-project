b41cc82bea2e3fa10e575ced968e52a3f9619ea9
Nadia Azim <nd.azim94@gmail.com>
2021-11-19 12:24:46 -0500

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import location from "./components/LocationPage/LocationPage";
+import Sort from "./components/Sort/Sort";
 import Home from "./pages/Home/Home";
+// import Bookings from "./components/Bookings/Bookings";
+
 // import Footer from './components/Footer/Footer';
 // import RatePage from '../../client/src/components/RatePage';
 
@@ -23,6 +26,7 @@ class App extends Component {
             <Route path="/location" exact component={location} />
             {/* <Route path= "/ratepage" component= {RatePage} /> */}
             {/* <Route path= "/bookings" exact component={Bookings}/> */}
+            <Route path="/sort" exact component={Sort} />
           </Switch>
           {/* <Footer /> */}
         </section>
